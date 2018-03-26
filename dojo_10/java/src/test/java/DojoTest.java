04a6aad9bd791fbb0b09f522f70303cd45113a79
Facundo D Aranno <facundo.daranno@mercadolibre.com>
2018-03-26 17:11:40 -0300

    [Dojo10]Solucion congrats

--- dojo_10/java/src/test/java/DojoTest.java
+++ dojo_10/java/src/test/java/DojoTest.java
@@ -42,7 +42,37 @@ public class DojoTest {
                 ((List<Map>)congrats.get("sections")).get(0).get("type"),
                 "offline_payment"
         );
+    }
 
+    @Test
+    public void test_congrats_order_paid_off_for_android_7_12_0_with_entities() throws IOException {
+        CongratsService congratsService = new FakeCongratsService();
+        Congrats expectedCongrats = new Congrats();
+
+	    DeviceInterface android712 = new Android712(congratsService);
+
+        Congrats congrats = android712.renderCongrats();
+
+        assertEquals(
+                expectedCongrats,
+                congrats
+        );
+    }
+
+    //@Smell("El assert conoce la implementación de congrats y esto viola el encapsulamiento")
+    @Test
+    public void test_congrats_order_paid_off_for_ios_8_10_0_with_entities() throws IOException {
+        CongratsService congratsService = new FakeCongratsService();
+        Congrats expectedCongrats = new CongratsExpectedForIOs810( "mlu_offline_payment" );
+
+        Ios810 ios810 = new Ios810( congratsService );
+
+        Congrats congrats = ios810.renderCongrats();
+
+        assertEquals(
+                expectedCongrats,
+                congrats
+        );
     }
 
 }

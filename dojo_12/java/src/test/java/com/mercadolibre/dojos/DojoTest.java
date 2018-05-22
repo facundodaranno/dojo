31084bb0e79c8a0a15ac54ac31b6718f25cbd8d2
Facundo D Aranno <facundo.daranno@mercadolibre.com>
2018-05-22 00:32:39 -0300

    solucion test 1

--- dojo_12/java/src/test/java/com/mercadolibre/dojos/DojoTest.java
+++ dojo_12/java/src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -13,23 +14,30 @@ import org.junit.Test;
  * Tests for the dojo.
  */
 public class DojoTest {
-	
-	private SpreadSheet sheet;
+
 	
 	@Before
 	public void setup() {
 	}
 	
 	@Test
-	@Ignore
 	public void se_solicita_un_aula_para_10_personas_debe_indicar_aula_b() {
 
-		// Listado de aulas:  [{nombre:aula_a, capacidad:5},{nombre:aula_b, capacidad:15}]
-		// ...
-        // Code here!
-        // ... 
+		// Listado de aulas:  [{nombre:aula_a, capacidadSolicitada:5},{nombre:aula_b, capacidadSolicitada:15}]
 
-		assertThat( result , "aula b");
+		Aula aulaA = new Aula("aula_a", 5);
+		Aula aulaB = new Aula("aula_b", 15);
+		Aula aulaB = new Aula("aula_c", 15);
+
+		ListadoAulas listadoAulas = new ListadoAulas();
+		listadoAulas.agregarAula( aulaA );
+		listadoAulas.agregarAula( aulaB );
+
+		SolicitudAula solicitudAula = new SolicitudAula(10);
+		Aula aulaDisponible = listadoAulas.realizarSolicitud( solicitudAula );
+
+		Aula aulaEsperada = new Aula("aula_b", 15);
+		Assert.assertTrue( aulaDisponible.equals( aulaEsperada ) );
 	}
 
 }
